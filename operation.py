04d32a6436a48db92913c44ad4b411f3b47072e3
iganari <cigaguri@gmail.com>
2018-09-21 14:23:04 +0900

    /tmpに対して、書き込み権限があるか確認する　

--- operation.py
+++ operation.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 # conding: utf-8
 
-print('Hello World')
+# print('Hello World')
+
+### /tmpに書き込み権限があるか確認する
+
+def chk_tmp_permission():
+    import os
+    
+    chk_write = os.access('/opt', os.W_OK)
+
+    if chk_write == True:
+        print('OK. You have Write Permisson')
+    else:
+        print('NO, You do not have Write Permisson')
+
+
+if __name__ == '__main__':
+    chk_tmp_permission()
