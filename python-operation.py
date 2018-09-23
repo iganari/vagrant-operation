a162a009bffd4cb0b5d78a9e29d4b285cd0a94f0
iganari <cigaguri@gmail.com>
2018-09-23 09:51:42 +0900

    modified chk_vbscript

--- python-operation.py
+++ python-operation.py
@@ -20,27 +20,25 @@ def chk_tmp_permission():
 def chk_vbscript():
     import sys
     
-    chk_vb_script = os.path.isfile('/usr/bin/VBoxManage')
-    vb_script_ubuntu = '/usr/bin/VBoxManage'
-    vb_script_macos  = '/usr/local/bin/VboxManage'
     paths = [ "/usr/bin/", "/usr/local/bin/" ]
 
     for path in paths: 
-        print(path, 'VBoxManage')
-
-        # if chk_vb_script == True:
-        #     pass
-        # else:
-        #     print('Maybe, You do not install Virtualbox (Not lock up VBoxManage !)')
-        #     sys.exit(0)
+        vm_path = path + 'VBoxManage'
+        # print(vm_path)
+        if vm_path == True:
+            break
+        else:
+            print('Maybe, You do not install Virtualbox ( We could not find ' + vm_path + ' )')
+    else:
+        print('You do not install Virtualbox. Bye!')
+        sys.exit(0)
 
 if __name__ == '__main__':
 
-    # /tmpに書き込み権限があるか確認する
+    # Confirm whether you have write permission for /tmp
     chk_tmp_permission()
 
-    # VirtualBoxがインストールしてあるか確認する
-    # 具体的には '/usr/bin/VBoxManage' が存在することを確認する
+    # Check if VBoxManage is installed
     chk_vbscript()
 
 
