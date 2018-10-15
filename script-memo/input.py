e6025279b3a95020398d19aa84ee3266ad96a2d5
iganari <cigaguri@gmail.com>
2018-10-16 08:53:15 +0900

    inputをテストする

---
+++ script-memo/input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+print('Plz tell me your first name')
+
+your_name = input('>> ')
+
+print('Hello !!', your_name)
---
+++ script-memo/readme.md
@@ -0,0 +1,9 @@
+# test
+
+## memo
+
++ Python v3でコンソールで対話するには `input`
+    + input.py
++ Python v2でコンソールで対話するためには `row_input`
+    + 未検証
+    + raw_input
