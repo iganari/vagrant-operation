ef66635fe1044ff39d4536f4a147e775a43652a3
iganari <cigaguri@gmail.com>
2018-12-11 15:51:55 +0900

    不要なスクリプトを解除

--- script-memo/input/python2_raw_input.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-print u'Plz tell me your first name'
-
-your_name = raw_input('>> ')
-
-print "Hello !! "+your_name
--- script-memo/list-blank/readme.md
+++
@@ -1,5 +0,0 @@
-# 配列が空か否かを確認したい
-
-## 参考
-
-https://qiita.com/yonedaco/items/d0f65ca3dad2e085a51d
