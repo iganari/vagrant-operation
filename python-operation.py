dbf25601d2b6c0cd21bf63083d9806abbbaa4bca
iganari <cigaguri@gmail.com>
2019-04-09 08:50:35 +0900

    modify wording 2

--- python-operation.py
+++ python-operation.py
@@ -243,7 +243,7 @@ def input_num():
         input_number = int(i)
         pass
     else:
-        print('入力された値が自然数でありません')
+        print('入力された値が自然数でありません: {}'.format(i))
         sys.exit(1)
 
     return input_number
